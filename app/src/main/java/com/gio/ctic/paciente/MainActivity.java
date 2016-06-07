9e93ae346cbd6d2319b6051b6610d4420f49aaf6
vgiovanny <vgiovanny.mondragon@uni.pe>
2016-06-07 17:05:38 -0500

    Initial commit

---
+++ app/src/main/java/com/gio/ctic/paciente/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gio.ctic.paciente;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Historiales(View view){
+        Intent intent = new Intent(this,Historiales.class);
+        startActivity(intent);
+
+    }
+}
